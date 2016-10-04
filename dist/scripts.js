40d7ebf58f80b7d6c9b129fb72afc01d95d54606
Raphael Müller <raaphii@gmail.com>
2016-10-04 14:55:38 +0200

    Moved files from /DefaultTemplates to root

---
+++ dist/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	/* mailencrypting */
+	setTimeout(function(){
+		$('[data-madr1]').each(function(){
+			$this = $(this);
+			maddr = $this.attr('data-madr1') + '@' + $this.attr('data-madr2') + '.' + $this.attr('data-madr3');
+			$this.after( '<a href="mailto:'+maddr+'">'+maddr+'</a>' );
+			$this.hide();
+		});
+	}, 500);
+
+	/* Fancybox */
+	if(jQuery().fancybox){
+		$(".fancybox").fancybox({
+			parent: "form:first",
+			padding: 1
+		});
+	}
+});
