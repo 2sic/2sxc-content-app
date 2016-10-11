9aa3da713a85ee20113d25bb46daecd55d840cd9
iJungleboy <daniel.mettler@2sic.com>
2016-10-11 17:56:38 +0200

    Loads of updates incl. google-maps inclusion / key

--- src/google-map.js
+++ src/google-map.js
@@ -1,74 +1,100 @@
 (function ($) {
+    // Change this GoogleApiKey. Get it on Google API Console (https://console.developers.google.com/apis/)
+    var googleApiKey = "AIzaSyAUbRHtu3k_fg3jDGk_qAatE5jA4bC_ndE";
+
+
+    // the init-code
+    function initGoogleMaps() {
+
+        var googleMapLoadDeferred = $.Deferred();
+
+        $.fn.toGoogleMap = function (options) {
+            var mapElement = this.get(0);
+
+            googleMapLoadDeferred.promise().then(function() {
+                
+                var settings = $.extend({
+                    position: {
+                        lat: 0,
+                        lng: 0
+                    },
+                    zoom: 8,
+                    mapTypeId: "HYBRID",
+                    infoWindowHtml: "",
+                    showInfoWindow: true
+                }, options);
+
+                var mapOptions = {
+                    zoom: settings.zoom,
+                    mapTypeControl: true,
+                    center: settings.position,
+                    zoomControl: true,
+                    scaleControl: true,
+                    scrollwheel: false,
+                    draggable: false,
+                    mapTypeId: google.maps.MapTypeId[settings.mapTypeId]
+                };
+
+                var map = new google.maps.Map(mapElement, mapOptions);
+
+                // Create Marker
+                var marker = new google.maps.Marker({
+                    position: settings.position,
+                    map: map
+                });
+
+                if(settings.infoWindowHtml && settings.infoWindowHtml !== '') {
+                    // Create InfoWindow
+                    var infoWindow = new google.maps.InfoWindow({
+                        content: settings.infoWindowHtml
+                    });
+                    // Add Event listener
+                    google.maps.event.addListener(marker, 'click', function () {
+                        infoWindow.open(map, marker);
+                    });
+                }
+
+                google.maps.event.addListenerOnce(map, 'idle', function () {
+                    if (settings.showInfoWindow)
+                        infoWindow.open(map, marker);
+                });
+
+            });
+
+            return this;
+        };
+
+        // Register google map load callback
+        window.googleMapLoadCallback = function() {
+            googleMapLoadDeferred.resolve(true);
+        };
+        
+        if(!window.googleMapsLoaded)
+        {
+            window.googleMapsLoaded = true;
+            $.getScript("http://maps.google.com/maps/api/js?key=" + googleApiKey + "&sensor=true&callback=googleMapLoadCallback");
+        }
+    }
+
+
+    // check if it's the original key, which shouldn't be used in live sites
+    // do not turn this off, it's important!
+    // to disable the warning, replace the API key as explained on https://github.com/2sic/2sxc-content-bootstrap3/wiki/google-maps-api-key
+    function showWarningIfDemoKeyIsUsed() {
+        var showApiKeyWarning = googleApiKey === "A-I-z-a-S-yAUbRHtu3k_fg3jDGk_qAatE5jA4bC_ndE".replace(/-/g, "");
+        console.log(showApiKeyWarning);
+        if(showApiKeyWarning){
+            $('.co-google-map-container').append('<p class="alert alert-danger googlemap-apiwarning"><strong>Warning:</strong> This map uses a demo API-Key, which will cause problems on live web sites. Change the GoogleApiKey using <a class="alert-link target="_blank" href="https://github.com/2sic/2sxc-content-bootstrap3/wiki/google-maps-api-key" target="_blank">these instructions</a></p>');
+        }
+    }
+
+    showWarningIfDemoKeyIsUsed();
+
+    // The google-maps code - this usually shougoldn't need changing
     if ($.fn.toGoogleMap)
         return;
     
-    var googleMapLoadDeferred = $.Deferred();
+    initGoogleMaps();
 
-    $.fn.toGoogleMap = function (options) {
-        var mapElement = this.get(0);
-
-        googleMapLoadDeferred.promise().then(function() {
-            
-            var settings = $.extend({
-                position: {
-                    lat: 0,
-                    lng: 0
-                },
-                zoom: 8,
-                mapTypeId: "HYBRID",
-                infoWindowHtml: "",
-                showInfoWindow: true
-            }, options);
-
-            var mapOptions = {
-                zoom: settings.zoom,
-                mapTypeControl: true,
-                center: settings.position,
-                zoomControl: true,
-                scaleControl: true,
-                scrollwheel: false,
-                draggable: false,
-                mapTypeId: google.maps.MapTypeId[settings.mapTypeId]
-            };
-
-            var map = new google.maps.Map(mapElement, mapOptions);
-
-            // Create Marker
-            var marker = new google.maps.Marker({
-                position: settings.position,
-                map: map
-            });
-
-            if(settings.infoWindowHtml && settings.infoWindowHtml !== '') {
-                // Create InfoWindow
-                var infoWindow = new google.maps.InfoWindow({
-                    content: settings.infoWindowHtml
-                });
-                // Add Event listener
-                google.maps.event.addListener(marker, 'click', function () {
-                    infoWindow.open(map, marker);
-                });
-            }
-
-            google.maps.event.addListenerOnce(map, 'idle', function () {
-                if (settings.showInfoWindow)
-                    infoWindow.open(map, marker);
-            });
-
-        });
-
-        return this;
-    };
-
-    // Register google map load callback
-    window.googleMapLoadCallback = function() {
-        googleMapLoadDeferred.resolve(true);
-    };
-    
-    if(!window.googleMapsLoaded)
-    {
-        window.googleMapsLoaded = true;
-        $.getScript("http://maps.google.com/maps/api/js?key=AIzaSyAUbRHtu3k_fg3jDGk_qAatE5jA4bC_ndE&sensor=true&callback=googleMapLoadCallback");
-    }
 
 } (jQuery));
