67594e286e73c6550dc348add84f4817dfb09d1f
Roman Opalko <roman.opalko@2sic.com>
2021-05-04 16:37:44 +0200

    refactor google maps

--- src/ts/google-maps.ts
+++ src/ts/google-maps.ts
@@ -1,113 +1,113 @@
 
 declare const google : any;
 
+const winAny = window as any;
+let googleApiKey = winAny.googleMapsApiKey;
+
+// the init-code
+function giveJqueryGoogleMaps() {
+  // if this had already been run, stop here
+  if (($.fn as any).toGoogleMap) {
+    return;
+  }
+
+  console.log('giveJqueryGoogleMaps');
+
+  var googleMapLoadDeferred = $.Deferred();
+
+  ($.fn as any).toGoogleMap = function (options: any) {
+      var mapElement = this.get(0);
+
+      googleMapLoadDeferred.promise().then(function () {
+
+          var settings = $.extend({
+              position: {
+                  lat: 0,
+                  lng: 0
+              },
+              zoom: 8,
+              mapTypeId: "HYBRID",
+              infoWindowHtml: "",
+              showInfoWindow: true
+          }, options);
+
+          var mapOptions = {
+              zoom: settings.zoom,
+              mapTypeControl: true,
+              center: settings.position,
+              zoomControl: true,
+              scaleControl: true,
+              scrollwheel: false,
+              mapTypeId: google.maps.MapTypeId[settings.mapTypeId]
+          };
+
+          var map = new google.maps.Map(mapElement, mapOptions);
+
+          // Create Marker
+          var marker = new google.maps.Marker({
+              position: settings.position,
+              map: map,
+              icon: settings.icon
+          });
+
+          if (settings.infoWindowHtml && settings.infoWindowHtml !== '') {
+              // Create InfoWindow
+              var infoWindow = new google.maps.InfoWindow({
+                  content: settings.infoWindowHtml
+              });
+              // Add Event listener
+              google.maps.event.addListener(marker, 'click', function () {
+                  infoWindow.open(map, marker);
+              });
+          }
+
+          google.maps.event.addListenerOnce(map, 'idle', function () {
+              if (settings.showInfoWindow)
+                  infoWindow.open(map, marker);
+          });
+
+      });
+
+      return this;
+  };
+
+  // Register google map load callback
+  winAny.googleMapLoadCallback = function () {
+      googleMapLoadDeferred.resolve(true);
+  };
+
+  if (!winAny.googleMapsLoaded) {
+    winAny.googleMapsLoaded = true;
+      getScript("//maps.google.com/maps/api/js?key=" + googleApiKey + "&sensor=true&callback=googleMapLoadCallback");
+  }
+}
+
+function getScript(source: string) {
+  var script = document.createElement('script');
+  var prior = document.getElementsByTagName('script')[0];
+  (script as any).async = 1;
+
+  (script as any).onload = (script as any).onreadystatechange = function( _: any, isAbort: any ) {
+      if(isAbort || !(script as any).readyState || /loaded|complete/.test((script as any).readyState) ) {
+          script.onload = (script as any).onreadystatechange = null;
+          script = undefined;
+      }
+  };
+
+  script.src = source;
+  prior.parentNode.insertBefore(script, prior);
+}
+
 /* Google Maps API Key */
 export function activateGoogleMaps() {
   
-  const winAny = window as any;
   // Change this GoogleApiKey. They are in the App-Settings. Read instructions here: https://azing.org/2sxc/r/ippFQYkz
-  let googleApiKey = winAny.googleMapsApiKey;
   if(!googleApiKey) return; 
 
   const googleMapsElem = $('.co-google-map-container');
 
   const showApiKeyWarning = googleApiKey.indexOf("warning!") > -1;
   googleApiKey = googleApiKey.replace("warning!", "");
-
-  // the init-code
-  function initGoogleMaps(elem: HTMLElement) {
-    // if this had already been run, stop here
-    if (($.fn as any).toGoogleMap) {
-        return;
-    }
-
-    console.log('initGoogleMaps');
-
-    var googleMapLoadDeferred = $.Deferred();
-
-    ($.fn as any).toGoogleMap = function (options: any) {
-        var mapElement = this.get(0);
-
-        googleMapLoadDeferred.promise().then(function () {
-
-            var settings = $.extend({
-                position: {
-                    lat: 0,
-                    lng: 0
-                },
-                zoom: 8,
-                mapTypeId: "HYBRID",
-                infoWindowHtml: "",
-                showInfoWindow: true
-            }, options);
-
-            var mapOptions = {
-                zoom: settings.zoom,
-                mapTypeControl: true,
-                center: settings.position,
-                zoomControl: true,
-                scaleControl: true,
-                scrollwheel: false,
-                mapTypeId: google.maps.MapTypeId[settings.mapTypeId]
-            };
-
-            var map = new google.maps.Map(mapElement, mapOptions);
-
-            // Create Marker
-            var marker = new google.maps.Marker({
-                position: settings.position,
-                map: map,
-                icon: settings.icon
-            });
-
-            if (settings.infoWindowHtml && settings.infoWindowHtml !== '') {
-                // Create InfoWindow
-                var infoWindow = new google.maps.InfoWindow({
-                    content: settings.infoWindowHtml
-                });
-                // Add Event listener
-                google.maps.event.addListener(marker, 'click', function () {
-                    infoWindow.open(map, marker);
-                });
-            }
-
-            google.maps.event.addListenerOnce(map, 'idle', function () {
-                if (settings.showInfoWindow)
-                    infoWindow.open(map, marker);
-            });
-
-        });
-
-        return this;
-    };
-
-    // Register google map load callback
-    winAny.googleMapLoadCallback = function () {
-        googleMapLoadDeferred.resolve(true);
-    };
-
-    if (!winAny.googleMapsLoaded) {
-      winAny.googleMapsLoaded = true;
-        getScript("//maps.google.com/maps/api/js?key=" + googleApiKey + "&sensor=true&callback=googleMapLoadCallback");
-    }
-  }
-
-  function getScript(source: string) {
-    var script = document.createElement('script');
-    var prior = document.getElementsByTagName('script')[0];
-    (script as any).async = 1;
-
-    (script as any).onload = (script as any).onreadystatechange = function( _: any, isAbort: any ) {
-        if(isAbort || !(script as any).readyState || /loaded|complete/.test((script as any).readyState) ) {
-            script.onload = (script as any).onreadystatechange = null;
-            script = undefined;
-        }
-    };
-
-    script.src = source;
-    prior.parentNode.insertBefore(script, prior);
-}
-
 
   // check if it's the original key, which shouldn't be used in live sites
   // do not turn this off, it's important!
@@ -123,8 +123,8 @@ export function activateGoogleMaps() {
 
     googleMapsElem.each(function() {
       showWarningIfDemoKeyIsUsed(this);
-
-      initGoogleMaps(this);
     })
   }
+
+  giveJqueryGoogleMaps();
 }
