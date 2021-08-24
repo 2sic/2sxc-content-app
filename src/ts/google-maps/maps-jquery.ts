385262d750810d3efb2ae9429e0082ac9779e95a
Roman Opalko <roman.opalko@2sic.com>
2021-08-24 16:31:36 +0200

    wip: remove jquery from maps

--- src/ts/google-maps/maps-jquery.ts
+++ src/ts/google-maps/maps-jquery.ts
@@ -6,65 +6,65 @@ export function registerToGoogleMap(mapsLoadedPromise: JQuery.Deferred<any, any,
   if(debug) console.log('giveJqueryMaps-before');
 
   // if this had already been run, stop here
-  if (($.fn as any).toGoogleMap) return;
+  // if (($.fn as any).toGoogleMap) return;
 
   if(debug) console.log('giveJqueryGoogleMaps-after check');
 
 
-  ($.fn as any).toGoogleMap = function (options: any) {
-      var mapElement = this.get(0);
+  (window as any).toGoogleMap = function (mapElem: HTMLElement, options: any) {
+    var mapElement = mapElem;
 
-        mapsLoadedPromise.promise().then(function () {
+    mapsLoadedPromise.promise().then(function () {
 
-          var settings = $.extend({
-              position: {
-                  lat: 0,
-                  lng: 0
-              },
-              zoom: 8,
-              mapTypeId: "HYBRID",
-              infoWindowHtml: "",
-              showInfoWindow: true
-          }, options);
+      var settings = $.extend({
+          position: {
+              lat: 0,
+              lng: 0
+          },
+          zoom: 8,
+          mapTypeId: "HYBRID",
+          infoWindowHtml: "",
+          showInfoWindow: true
+      }, options);
 
-          var mapOptions = {
-              zoom: settings.zoom,
-              mapTypeControl: true,
-              center: settings.position,
-              zoomControl: true,
-              scaleControl: true,
-              scrollwheel: false,
-              mapTypeId: google.maps.MapTypeId[settings.mapTypeId]
-          };
+      var mapOptions = {
+          zoom: settings.zoom,
+          mapTypeControl: true,
+          center: settings.position,
+          zoomControl: true,
+          scaleControl: true,
+          scrollwheel: false,
+          mapTypeId: google.maps.MapTypeId[settings.mapTypeId]
+      };
 
-          var map = new google.maps.Map(mapElement, mapOptions);
+      var map = new google.maps.Map(mapElement, mapOptions);
 
-          // Create Marker
-          var marker = new google.maps.Marker({
-              position: settings.position,
-              map: map,
-              icon: settings.icon
-          });
-
-          if (settings.infoWindowHtml && settings.infoWindowHtml !== '') {
-              // Create InfoWindow
-              var infoWindow = new google.maps.InfoWindow({
-                  content: settings.infoWindowHtml
-              });
-              // Add Event listener
-              google.maps.event.addListener(marker, 'click', function () {
-                  infoWindow.open(map, marker);
-              });
-          }
-
-          google.maps.event.addListenerOnce(map, 'idle', function () {
-              if (settings.showInfoWindow)
-                  infoWindow.open(map, marker);
-          });
-
+      // Create Marker
+      var marker = new google.maps.Marker({
+          position: settings.position,
+          map: map,
+          icon: settings.icon
       });
 
-      return this;
+      if (settings.infoWindowHtml && settings.infoWindowHtml !== '') {
+          // Create InfoWindow
+          var infoWindow = new google.maps.InfoWindow({
+              content: settings.infoWindowHtml
+          });
+          // Add Event listener
+          google.maps.event.addListener(marker, 'click', function () {
+              infoWindow.open(map, marker);
+          });
+      }
+
+      google.maps.event.addListenerOnce(map, 'idle', function () {
+          if (settings.showInfoWindow)
+              infoWindow.open(map, marker);
+      });
+
+    });
+
+    return mapElem;
   };
 
   if(debug) console.log('giveJqueryGoogleMaps-after');
