4b076266cad10b6ed5092d9822bfae00a6deecd0
iJungleboy <daniel.mettler@2sic.com>
2023-12-07 17:53:02 +0100

    locations

--- bs5/Location/LocationHelper.cs
+++ bs5/Location/LocationHelper.cs
@@ -1,21 +1,18 @@
-using System.Collections.Generic;
-using System.Dynamic;
-using System.IO;
-using System.Globalization;
-using ToSic.Razor.Blade;
+using ToSic.Sxc.Data;
 
-public class LocationHelper: Custom.Hybrid.Code14
+public class LocationHelper: Custom.Hybrid.CodeTyped
 {
   // check a link, prepare target window, icon etc. based on various settings
-  public dynamic MapInfos(dynamic content) {
+  public object MapInfos(ITypedItem content) {
 
     // Language is used for the map-link
-    var language = CmsContext.Culture.CurrentCode.Split(new[] { '-' })[0];
+    // var content = AsItem(dynContent as object);
+    var language = MyContext.Culture.CurrentCode.Split(new[] { '-' })[0];
 
     // GPS is a JSON field, so we must use AsDynamic to access the properties
-    var gps = AsDynamic(content.GPS);
-    var gpsLong = Kit.Convert.ToDouble(gps.Longitude, fallback: 0);
-    var gpsLat = Kit.Convert.ToDouble(gps.Latitude, fallback: 0);
+    var gps = Kit.Json.ToTyped(content.String("GPS"));
+    var gpsLong = gps.Double("Longitude", fallback: 0);
+    var gpsLat = gps.Double("Latitude", fallback: 0);
 
     // this link will be used to open the Google-Directions in a new window
     var directionurl = gpsLong > 0
@@ -23,15 +20,15 @@ public class LocationHelper: Custom.Hybrid.Code14
       ? "https://www.google.com/maps/dir/" + Kit.Convert.ForCode(gpsLat) + "," + Kit.Convert.ForCode(gpsLong)
       // otherwise use the address
       : "https://maps.google.com/maps?daddr="
-        + (content.Street + " " + content.ZipCode + " " + content.City + " " + content.Country)
+        + (content.Get("Street") + " " + content.Get("ZipCode") + " " + content.Get("City") + " " + content.Get("Country"))
           .Replace(" ", "+")
         + "&amp;saddr=&amp;f=d&amp;hl=" + language + "&amp;ie=UTF8&amp;z=16";
 
-    return AsDynamic(new {
+    return new {
       GpsLong = gpsLong,
       GpsLat = gpsLat,
       DirectionUrl = directionurl,
-    });
+    };
   }
   
 }
