98fb1255059f5cbdac6a54824ea85af14e6c7910
iJungleboy <daniel.mettler@2sic.com>
2024-01-19 11:52:58 +0100

    locations new API

---
+++ ThisApp/Code/Location.cs
@@ -0,0 +1,60 @@
+using ToSic.Sxc.Context;
+
+namespace ThisApp.Data
+{
+  public partial class Location : Custom.Data.Item16Experimental
+  {
+    public string Company => String(fallback: "");
+
+    public string Description => String(fallback: "");
+    public string Street => String(fallback: "");
+    public string ZipCode => String(fallback: "");
+    public string City => String(fallback: "");
+    public string Country => String(fallback: "");
+    public string Tel => String(fallback: "");
+    public string Fax => String(fallback: "");
+    public string Mail => String(fallback: "");
+    public string Gps => String(fallback: "");
+  }
+
+  public partial class Location
+  {
+    public GpsCoordinates GpsCoordinates => _gpsCoordinates ?? (_gpsCoordinates = ParseGps(Gps));
+    private GpsCoordinates _gpsCoordinates;
+
+    private GpsCoordinates ParseGps(string gpsJson)
+    {
+      // GPS is a JSON field, so we must use the Json.ToTyped to access the properties
+      var coords = Kit.Json.ToTyped(gpsJson, propsRequired: false);
+      return new GpsCoordinates {
+        Latitude = coords.Double("Latitude"),
+        Longitude = coords.Double("Longitude")
+      };
+    }
+
+    public string RoutingUrl(ICmsContext context)
+    {
+      // Language is used for the map-link
+      // var content = AsItem(dynContent as object);
+      var language = context.Culture.CurrentCode.Split(new[] { '-' })[0];
+
+      var gps = GpsCoordinates;
+      // this link will be used to open the Google-Directions in a new window
+      return gps.Longitude > 0
+        // if we have coordinates, use them
+        ? "https://www.google.com/maps/dir/" + Kit.Convert.ForCode(gps.Latitude) + "," + Kit.Convert.ForCode(gps.Longitude)
+        // otherwise use the address
+        : "https://maps.google.com/maps?daddr="
+          + (Street + " " + ZipCode + " " + City + " " + Country).Replace(" ", "+")
+          + "&amp;saddr=&amp;f=d&amp;hl=" + language + "&amp;ie=UTF8&amp;z=16";
+    }
+
+  }
+
+  public class GpsCoordinates
+  {
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+  }
+
+}
