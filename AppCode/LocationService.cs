2ef45cd38ddc95bdb467d840c4a08a6c8229888b
iJungleboy <daniel.mettler@2sic.com>
2024-02-22 13:27:44 +0100

    move / create LocationService

--- AppCode/LocationService.cs
+++ AppCode/LocationService.cs
@@ -1,24 +1,27 @@
-using ToSic.Sxc.Context;
+using AppCode.Data;
 
-namespace AppCode.Data
+namespace AppCode
 {
-  // TODO: @2dm - review use of context and Kit!
-  public partial class Location
+  /// <summary>
+  /// Simple service to encrypt eMail addresses in the resulting HTML.
+  /// To use, must be created using GetService<ProtectMailService>() in your view, as it needs the Kit.
+  /// </summary>
+  public class LocationService: Custom.Hybrid.CodeTyped
   {
-    public string RoutingUrl(ICmsContext context)
+    public string RoutingUrl(Location loc)
     {
       // Language is used for the map-link
       // var content = AsItem(dynContent as object);
-      var language = context.Culture.CurrentCode.Split(new[] { '-' })[0];
+      var language = MyContext.Culture.CurrentCode.Split(new[] { '-' })[0];
 
-      var gps = GPS;
+      var gps = loc.GPS;
       // this link will be used to open the Google-Directions in a new window
       return gps.Longitude > 0
         // if we have coordinates, use them
         ? "https://www.google.com/maps/dir/" + Kit.Convert.ForCode(gps.Latitude) + "," + Kit.Convert.ForCode(gps.Longitude)
         // otherwise use the address
         : "https://maps.google.com/maps?daddr="
-          + (Street + " " + ZipCode + " " + City + " " + Country).Replace(" ", "+")
+          + $"{loc.Street} {loc.ZipCode} {loc.City} {loc.Country}".Replace(" ", "+")
           + "&amp;saddr=&amp;f=d&amp;hl=" + language + "&amp;ie=UTF8&amp;z=16";
     }
 
