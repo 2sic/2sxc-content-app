42050746bee17ee13fd4ea6f5d91a4fe472c2a80
iJungleboy <daniel.mettler@2sic.com>
2024-03-05 19:30:42 +0100

    wip move fancybox to base class

--- AppCode/Razor/AppRazor.Generated.cs
+++ AppCode/Razor/AppRazor.Generated.cs
@@ -22,10 +22,5 @@ namespace AppCode.Razor
     public new IAppTyped<AppSettings, AppResources> App => _app ??= Customize.App<AppSettings, AppResources>();
     private IAppTyped<AppSettings, AppResources> _app;
     
-    /// <summary>
-    /// Special helpers for titles, toolbars etc.
-    /// </summary>
-    protected ContentHelpers Helpers => _helpers ??= GetService<ContentHelpers>();
-    private ContentHelpers _helpers;
   }
 }
